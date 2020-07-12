7db24b64e9051e742c80d8bd2d2c92bd03cb9af6
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2020-07-12 18:39:22 -0400

    Add more docstr

--- src/bin/dnld_pmids.py
+++ src/bin/dnld_pmids.py
@@ -10,21 +10,27 @@ from pmidcite.pubmedqueryicite import PubMedQueryToICite
 
 def main():
     """Download PMIDs returned from user queries. Write: ./log/pmids ./log/icite"""
-    obj = PubMedQueryToICite(force_dnld=True, prt_icitepy=None)
     # pylint: disable=bad-whitespace
     queries = [
-        # Output filenames      PubMed query
-        # -----------------    -----------------------------------
+        # Output filenames         PubMed query
+        # -----------------       -----------------------------------
         ('systematic_review.txt', 'systematic review AND "how to"[TI]'),
     ]
-    # Default is to only run the last entry in the list, index = -1
+
+    # By default, only the last entry in the list is run.
+    # This allows you to build a history of searches,
+    # but not run all of them every time.
     #
-    # To run all entries in the list:
+    # To re-run all entries in the list:
     #   $ src/bin/dnld_pmids.py all
     #
-    # To run the last query:
-    #   $ src/bin/dnld_pmids.py -1
+    # To run the first query:
+    #   $ src/bin/dnld_pmids.py 0
     #
+    # To run the second to last query:
+    #   $ src/bin/dnld_pmids.py -2
+    #
+    obj = PubMedQueryToICite(force_dnld=True, prt_icitepy=None)
     dnld_idx = obj.get_index(sys.argv)
     obj.run(queries, dnld_idx)
 
