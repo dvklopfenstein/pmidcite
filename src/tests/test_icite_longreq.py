2e635cb7c10e38c0abdf50ebda55c5b22ee1a542
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2021-07-30 10:14:37 -0400

    Add beginnings of test for long requests

---
+++ src/tests/test_icite_longreq.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Test long request to https://icite.od.nih.gov/api/pubs"""
+
+
+def test_icite_longreq:
+    """Test long request to https://icite.od.nih.gov/api/pubs"""
+    pmid = 17717599
+    die
+
+
+if __name__ == '__main__':
+    test_icite_longreq()
+
+# Copyright (C) 2021-present, DV Klopfenstein. All rights reserved.
