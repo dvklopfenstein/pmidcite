34750f67dd9ba5b94af5b38484dd179361c9531a
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2022-11-26 11:14:02 -0500

    Add console_script for icite script; https://github.com/dvklopfenstein/pmidcite/issues/43

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ from os.path import dirname
 from os.path import join
 # from distutils.core import setup
 from glob import glob
-from os.pathlib import PurePosixPath
 from setuptools import setup
 # import versioneer
 
@@ -26,7 +25,7 @@ PACKAGES = [
     'pmidcite.plot',
 ]
 
-PACKAGE_DIRS = {p:join('src', PurePosixPath(*p.split('.'))) for p in PACKAGES}
+PACKAGE_DIRS = {p:join('src', *p.split('.')) for p in PACKAGES}
 
 def get_long_description():
     """Return the contents of the README.md as a string"""
@@ -38,14 +37,21 @@ def get_long_description():
 setup(
     name=NAME,
     ## version=versioneer.get_version(),
-    version='0.0.37',
+    version='0.0.38',
     author='DV Klopfenstein, PhD',
     author_email='dvklopfenstein@protonmail.com',
     ## cmdclass=versioneer.get_cmdclass(),
     packages=PACKAGES,
     package_dir=PACKAGE_DIRS,
     # https://stackoverflow.com/questions/18787036/difference-between-entry-points-console-scripts-and-scripts-in-setup-py
+    # https://stackoverflow.com/questions/45114076/python-setuptools-using-scripts-keyword-in-setup-py
+    # https://packaging.python.org/en/latest/guides/distributing-packages-using-setuptools/#scripts
     scripts=glob('src/bin/*.py'),
+    entry_points={
+        'console_scripts':[
+            'icite=src.bin.icite:main',
+        ],
+    },
     # https://pypi.org/classifiers/
     classifiers=[
         'Programming Language :: Python',
