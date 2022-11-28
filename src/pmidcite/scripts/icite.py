7413664c3800475230d0dff6ddb575ae38e4a19a
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2022-11-28 10:37:42 -0500

    Move script modules into new pmidcite.scripts; https://github.com/dvklopfenstein/pmidcite/issues/42

---
+++ src/pmidcite/scripts/icite.py
@@ -0,0 +1,15 @@
+"""Given a PubMed ID (PMID), return a list of citing publications"""
+
+__copyright__ = "Copyright (C) 2019-present, DV Klopfenstein. All rights reserved."
+__author__ = "DV Klopfenstein"
+
+from pmidcite.cli.icite import NIHiCiteCli  # get_argparser
+from pmidcite.cfg import get_cfgparser
+
+
+def main():
+    """Print NIH citation data for requested papers specified with PubMedIDs (PMIDs)"""
+    NIHiCiteCli(get_cfgparser(prt=None)).cli()
+
+
+# Copyright (C) 2019-present, DV Klopfenstein. All rights reserved.
