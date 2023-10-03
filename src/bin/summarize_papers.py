7bbbdaa9fd52b29dc84084c984048d6b9ec956f4
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2023-10-03 10:30:27 -0400

    update summarize_papers location

--- src/bin/summarize_papers.py
+++ src/bin/summarize_papers.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 """Summarize NIH citation data for requested papers from the commandline or in files"""
 
-__copyright__ = "Copyright (C) 2022-present, DV Klopfenstein, PhD, PhD. All rights reserved."
-__author__ = "DV Klopfenstein, PhD, PhD"
+__copyright__ = "Copyright (C) 2022-present, DV Klopfenstein, PhD. All rights reserved."
+__author__ = "DV Klopfenstein, PhD"
 
-from pmidcite.cli.summarize_papers import SummarizePapersCli # get_argparser
-from pmidcite.cfg import get_cfgparser
+from pmidcite.scripts.summarize_papers import main
 
-
-def main():
-    """Summarize NIH citation data for requested papers from the commandline or in files"""
-    SummarizePapersCli(get_cfgparser(prt=None)).cli()
 
 if __name__ == '__main__':
     main()
 
-# Copyright (C) 2022-present, DV Klopfenstein, PhD, PhD. All rights reserved.
+# Copyright (C) 2022-present, DV Klopfenstein, PhD. All rights reserved.
