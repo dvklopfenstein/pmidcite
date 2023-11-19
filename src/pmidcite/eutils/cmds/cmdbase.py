3cd6c6182f77eda064754bb9ba38e3731c3c7b0f
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2023-11-19 18:22:18 -0500

    clean up

--- src/pmidcite/eutils/cmds/cmdbase.py
+++ src/pmidcite/eutils/cmds/cmdbase.py
@@ -8,19 +8,17 @@ from pmidcite.cfg import Cfg
 from pmidcite.eutils.cmds.base import EntrezUtilities
 
 
-#### class EntrezCommands(EntrezUtilities):
 class CommandBase(EntrezUtilities):
     """Fetch and write text"""
 
-    def __init__(self, retmax=10000, rettype='medline', retmode='text', batch_size=100, **kws):
-        kws_base = {k:v for k, v in kws.items() if k in EntrezUtilities.exp_kws}
+    def __init__(self, retmax=10000, rettype='medline', retmode='text', batch_size=100):
         cfg = Cfg()
-        super(CommandBase, self).__init__(
-            cfg.get_email(), cfg.get_apikey(), cfg.get_tool(), **kws_base)
+        super(CommandBase, self).__init__(cfg.get_email(), cfg.get_apikey(), cfg.get_tool())
         self.batch_size = batch_size
         self.retmax = retmax
         self.rettype = rettype
         self.retmode = retmode
+        ## print(f'CommandBase: retmax({retmax}) retmode({retmode}) rettype({rettype})')
 
 
 # Copyright (C) 2016-present DV Klopfenstein, PhD. All rights reserved.
