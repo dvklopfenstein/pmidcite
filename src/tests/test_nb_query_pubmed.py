1a407fccdae5879c254ac063ca1e2b3cdb1b5b34
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2024-02-04 13:43:33 -0500

    Make test clearer

--- src/tests/test_nb_query_pubmed.py
+++ src/tests/test_nb_query_pubmed.py
@@ -18,41 +18,41 @@ def test_query_pubmed_apikey1():
     """Test notebook that queries PubMed with free text"""
     cfg = Cfg(prt_fullname=False)
 
-    # ## Instantiate a PubMed object
-    # In[2]:
+    # Instantiate a PubMed object
     pmobj = PubMed(
         email=cfg.get_email(),
         apikey=cfg.get_apikey(),
         tool=cfg.get_tool())
 
-    # ## Query PubMed, download PMIDs
-    # In[3]:
+    _run(pmobj, force_download=False)
+    #_run(pmobj, force_download=True)
+
+
+def _run(pmobj, force_download):
+    # Query PubMed, download PMIDs
     pubmed_query = 'Orcinus Orca Type D'
     pmids = pmobj.dnld_query_pmids(pubmed_query)
+    print('', flush=True)
+    print(f'TEST QUERY:            {pubmed_query}', flush=True)
+    print(f'TEST PMIDS DOWNLOADED: {pmids}', flush=True)
 
-    # ## Print NIH citation data for the papers
-    # In[4]:
+    # Print NIH citation data for the papers
     dnldr = get_downloader()
     pmid2paper = dnldr.get_pmid2paper(pmids)
     for paper in pmid2paper.values():
-        print(paper)
+        print(f'TEST PAPER: {paper}', flush=True)
 
-
-    # ## Download the PubMed abstract
     # Download the PubMed abstract for the newest paper
-    # In[5]:
     paper_chosen = sorted(pmid2paper.values(), key=lambda o: o.icite.dct['year'])[0]
-    print(paper_chosen)
+    print(f'\nTEST CHOSEN: {paper_chosen}\n', flush=True)
 
-    # In[6]:
-    force_download = False
     pmid2nt = pmobj.dnld_wr1_per_pmid([paper_chosen.pmid], force_download, dir_pubmed_txt=".")
-    for pmid, data in pmid2nt.items():
-        print('PMID', pmid, data)
 
-    # In[7]:
-    file_pubmed = './pubmed_{PMID}.txt'.format(PMID=paper_chosen.pmid)
-    system('cat {TXT}'.format(TXT=file_pubmed))
+    for pmid, data in pmid2nt.items():
+        print('TEST PMID', pmid, data, flush=True)
+
+    file_pubmed = f'./pubmed_{paper_chosen.pmid}.txt'
+    system(f'cat {file_pubmed}')
 
 
 if __name__ == '__main__':
